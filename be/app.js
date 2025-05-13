0d391a73de3e11152b4dcb5a231b80408011754a
김병곤 <gon00130@gmail.com>
2025-05-13 16:03:23 +0900

    도서api 확인 테스트

--- be/app.js
+++ be/app.js
@@ -3,12 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var indexRouter = require('./routes/index'); // 추가
-
+const booksRouter = require('./routes/book');
+require('dotenv').config();
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use('/api/book', booksRouter);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
