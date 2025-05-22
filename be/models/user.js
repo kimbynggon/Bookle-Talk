19a33806d0f498ea280bd453ff2e3177e05518a9
김병곤 <gon00130@gmail.com>
2025-05-22 14:45:08 +0900

    ㄴ

--- be/models/user.js
+++ be/models/user.js
@@ -1,62 +1,36 @@
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    username: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-      unique: true
-    },
-    email: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-      unique: true
-    },
-    password: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    },
-    created_at: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW
-    }
-  }, {
-    tableName: 'users',
-    timestamps: false,  // timestamps를 false로 변경
-    // createdAt과 updatedAt 설정 제거
-    underscored: true
-  });
-
-  // 관계 설정 부분은 그대로 유지
-  User.associate = function(models) {
-    User.hasMany(models.Review, {
-      foreignKey: 'user_id',
-      as: 'reviews'
-    });
-
-    User.hasMany(models.Bookmark, {
-      foreignKey: 'user_id',
-      as: 'bookmarks'
-    });
-
-    User.hasMany(models.Like, {
-      foreignKey: 'user_id',
-      as: 'likes'
-    });
-
-    User.hasMany(models.Chat, {
-      foreignKey: 'user_id',
-      as: 'chats'
-    });
-
-    User.hasMany(models.ReviewComment, {
-      foreignKey: 'user_id',
-      as: 'comments'
-    });
+    const User = sequelize.define(
+      "User",
+      {
+        user_id: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+        },
+        password: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        nickname: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+        },
+        email: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+      },
+      {
+        tableName: "users",
+        underscored: true,
+        timestamps: true,
+        createdAt: "created_at",
+        updatedAt: false,
+        paranoid: false,
+      }
+    );
+  
+    return User;
   };
-
-  return User;
-};
+  
