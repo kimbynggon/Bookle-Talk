2704ae53579b1bd8bd1941f9626f43017a4ec10a
YHW <usus0175@gmail.com>
2025-05-26 16:24:07 +0900

    like.js 오류수정

--- be/models/like.js
+++ be/models/like.js
@@ -1,22 +1,34 @@
-const model = require("sequelize");
+const { Model } = require('sequelize'); // ✅ 수정된 부분
 
 module.exports = (sequelize, DataTypes) => {
   class Like extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      // 관계 정의 가능
+      Like.belongsTo(models.User, { foreignKey: 'user_id' });
+      Like.belongsTo(models.Book, { foreignKey: 'book_id' });
     }
   }
-  Like.init({
-    user_id: DataTypes.INTEGER,
-    book_id: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Like',
-  });
+
+  Like.init(
+    {
+      user_id: DataTypes.INTEGER,
+      book_id: DataTypes.INTEGER
+    },
+    {
+      sequelize,
+      modelName: 'Like',
+      tableName: 'likes',
+      timestamps: true,
+      underscored: true,
+      paranoid: true,
+      indexes: [
+        {
+          unique: true,
+          fields: ['user_id', 'book_id']
+        }
+      ]
+    }
+  );
+
   return Like;
 };
