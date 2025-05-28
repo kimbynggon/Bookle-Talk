8c7912167eefa39587e2af58ab5b1042ec44ad6d
so0oppy <hansb102318@gmail.com>
2025-05-28 14:47:16 +0900

    [fix] 모델 수정

--- be/models/chat.js
+++ be/models/chat.js
@@ -1,8 +1,22 @@
 module.exports = (sequelize, DataTypes) => {
   const Chat = sequelize.define('Chat', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    book_id: { type: DataTypes.INTEGER, allowNull: false },
-    user_id: { type: DataTypes.INTEGER, allowNull: false },
+    book_id: { 
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+      references: {
+        model: 'Books',
+        key: 'id'
+      }
+    },
+    user_id: { 
+      type: DataTypes.STRING, 
+      allowNull: false,
+      references: {
+        model: 'Users', 
+        key: 'user_id'        
+      }
+    },
     message: { type: DataTypes.TEXT, allowNull: false },
     created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
   }, {
@@ -13,8 +27,11 @@ module.exports = (sequelize, DataTypes) => {
 
   Chat.associate = function(models) {
     Chat.belongsTo(models.Book, { foreignKey: 'book_id' });
-    Chat.belongsTo(models.User, { foreignKey: 'user_id' });
+    Chat.belongsTo(models.User, { 
+      foreignKey: 'user_id',
+      targetKey: 'user_id', 
+      as: 'user'
+    });
   };
-
   return Chat;
 };
